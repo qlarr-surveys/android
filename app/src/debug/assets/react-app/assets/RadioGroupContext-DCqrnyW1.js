d6f114de8f1249e385a36df299b8962ee8346b54
Amr Askoura <askoura@qlarr..com>
2025-05-25 20:56:04 +0200

    fix slow survey loading

---
+++ app/src/debug/assets/react-app/assets/RadioGroupContext-DCqrnyW1.js
@@ -0,0 +1,6 @@
+import { r as reactExports } from "./index-DRrn8_Vw.js";
+const RadioGroupContext = /* @__PURE__ */ reactExports.createContext(void 0);
+export {
+  RadioGroupContext as R
+};
+//# sourceMappingURL=RadioGroupContext-DCqrnyW1.js.map
--- app/src/debug/assets/react-app/assets/RadioGroupContext-DvO4ZmII.js
+++
@@ -1,6 +0,0 @@
-import { r as reactExports } from "./index-D72a2Oud.js";
-const RadioGroupContext = /* @__PURE__ */ reactExports.createContext(void 0);
-export {
-  RadioGroupContext as R
-};
-//# sourceMappingURL=RadioGroupContext-DvO4ZmII.js.map
