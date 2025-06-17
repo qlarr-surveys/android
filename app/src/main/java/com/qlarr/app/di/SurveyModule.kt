f085efb8f54fdffc1f96f3832b7b9912a40192e2
JosipK <komparic.josip@gmail.com>
2025-06-17 20:47:36 +0200

    cleanup of unused classes

--- app/src/main/java/com/qlarr/app/di/SurveyModule.kt
+++ app/src/main/java/com/qlarr/app/di/SurveyModule.kt
@@ -1,7 +1,5 @@
 package com.qlarr.app.di
 
-import com.qlarr.app.business.survey.SurveyData
-import com.qlarr.app.ui.survey.RunSurveyViewModel
 import com.qlarr.app.ui.survey.SurveyViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -10,8 +8,5 @@ val surveyModule =
     module {
         viewModel { (surveyId: String) ->
             SurveyViewModel(surveyId, get(), get(), get())
-        }
-        viewModel { (survey: SurveyData) ->
-            RunSurveyViewModel(survey, get())
         }
     }
